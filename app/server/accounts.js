37bf98bb624c9fe074b220e31fbe4cb41efb3ec9
Daniel Rosenberg <dan@littlegenius.io>
2015-09-22 09:29:46 +0800

    config not hardcoded fixes #25

--- app/server/accounts.js
+++ app/server/accounts.js
@@ -9,23 +9,18 @@ ServiceConfiguration.configurations.remove({
 });
 
 ServiceConfiguration.configurations.insert({
-  service: "google",
-  clientId: "621918568251-nf0apn9k6s71808cvp7v5j1ciuluk6vr.apps.googleusercontent.com",
-  secret: "qZ5NcbllTG4Hy_gkGiZ9mKkT"
+    service:  "google",
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    secret:   process.env.GOOGLE_SECRET
 });
 
 
 Accounts.onCreateUser(function (options, user) {
-
   // analytics.track("Sign Up", {
   //   date: new Date(),
   // });
-
-
   user.profile = options.profile ? options.profile : {};
-
   user.profile = lodash.assign(Schema.profile, user.profile);
-
 
   if (user.services.hasOwnProperty('google')) {
     user.emails = [];
@@ -34,14 +29,12 @@ Accounts.onCreateUser(function (options, user) {
     });
     user.profile.firstname = user.services.google.given_name;
     user.profile.lastname = user.services.google.family_name;
-
-
-  } else {
+  }
+  else {
     // we wait for Meteor to create the user before sending an email
     Meteor.setTimeout(function () {
       Accounts.sendVerificationEmail(user._id);
     }, 2 * 1000);
-
   }
   return user;
 });
--- app/server/mandrill.js
+++ app/server/mandrill.js
@@ -8,9 +8,9 @@
  });*/
 
 Mandrill.config({
-  username: "dan@littlegenius.io",  // the email address you l""og into Mandrill with. Only used to set MAIL_URL.
-  key: "GBt6d5ggNEP7969s9j9DyA",  // get your Mandrill key from https://mandrillapp.com/settings/index
-  port: 587,  // defaults to 465 for SMTP over TLS
+  username: process.env.MANDRILL_USERNAME,   // the email address you l""og into Mandrill with. Only used to set MAIL_URL.
+  key: process.env.MANDRILL_KEY,   // the email address you l""og into Mandrill with. Only used to set MAIL_URL.
+  port: process.env.MANDRILL_PORT  // defaults to 465 for SMTP over TLS
   // host: 'smtp.mandrillapp.com',  // the SMTP host
   // baseUrl: 'https://mandrillapp.com/api/1.0/'  // update this in case Mandrill changes its API endpoint URL or version
 });
