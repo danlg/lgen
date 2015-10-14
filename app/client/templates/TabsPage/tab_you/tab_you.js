1a6d89971828f6b55199bed2abacf1f5d12aae82
Daniel Rosenberg <dan@littlegenius.io>
2015-10-14 15:27:29 +0800

    recommendation added for Student

--- app/client/templates/TabsPage/tab_you/tab_you.js
+++ app/client/templates/TabsPage/tab_you/tab_you.js
@@ -54,15 +54,23 @@ Template.TabYou.events({
   'click .love': function (argument) {
     if (Meteor.isCordova) {
       switch (Meteor.user().profile.role) {
+        //TODO localization English hardcoded for recommendation, with template i18n, see doc on how to insert variable
         case "Teacher":
           var text = "Hey! \r\n\r\nI have been using Little Genius to text my students and parents without sharing my " +
             "personal phone number.\r\nYou have to try it! It saves time, students love it and it is free! " +
             "\r\nHere is the link: " + Meteor.settings.public.WEB_URL + "?rid=" + Meteor.userId();
           break;
-        case "Parent":
+        case "Student":
           var text = "Hey!\r\n\r\nMy teachers have been using the Little Genius app to message our class before " +
             "assignments are due, share photos and update us with last minute changes. You should tell your teachers about it! " +
             "It\'s really helpful and free.\r\nHere is the link: " + Meteor.settings.public.WEB_URL + "?rid=" + Meteor.userId() + "\"\r\n";
+          break;
+        case "Parent":
+          var text = "Hey!\r\n\r\nOur school teachers have been using the Little Genius app to message our class before " +
+            "assignments are due, share photos and update us with last minute changes. You should tell your school teachers about it! " +
+            "It\'s really helpful and free.\r\nHere is the link: " + Meteor.settings.public.WEB_URL + "?rid=" + Meteor.userId() + "\"\r\n";
+          break;
+
         default:
       }
       window.plugins.socialsharing.share(text, null, null, null);
@@ -75,7 +83,7 @@ Template.TabYou.events({
           console.log("error", error);
         }
       });
-    }
+    }//end of isCordova
   }
 });
 
