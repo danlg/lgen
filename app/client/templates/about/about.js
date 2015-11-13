a382fb951fdab3f1a61eff4369c442c5502eba50
Daniel Rosenberg <dan@littlegenius.io>
2015-11-13 19:24:22 +0800

    kill dead code

--- app/client/templates/about/about.js
+++ app/client/templates/about/about.js
@@ -35,82 +35,10 @@ Template.ionNavBar.events({
 });
 
 Template.About.events({
-  //'click .about': function (argument) {
-  //  if (Meteor.isCordova) {
-  //    var pattern = /-.*/g;
-  /*    navigator.globalization.getPreferredLanguage(
-        function (language) {
-          // alert('language: ' + language.value + '\n');
-          log.info(language);
-          var lang = language.value.replace(pattern, "");
 
-          log.info(lang);
-          // var html  = "http://esprit.io/legal/"+lang+".privacy.html";
-          // var html = Meteor.call("getPpLink",lang);
-          Meteor.call("getPpLink", lang, function (error, result) {
-            if (error) {
-              log.error("error", error);
-            }
-            else {
-              switch (window.device.platform) {
-                case "Android":
-                  navigator.app.loadUrl(result, {openExternal: true});
-                  break;
-                case "iOS":
-                  window.open(result, '_system');
-                  break;
-              }
-              // return legalLink.set(result);
-            }
-          });
-          // return html;
-        },
-        function () {
-          alert('Error getting language\n');
-        }
-      );
-    } else {
-      log.info('you are not in phone');
-    }
-  }*/
 });
 Template.About.rendered = function () {
-  //if (Meteor.isCordova) {
-  //  var pattern = /-.*/g;
-  /*
-    navigator.globalization.getPreferredLanguage(
-      function (language) {
-        // alert('language: ' + language.value + '\n');
-        // log.info(language);
-        var lang = language.value.replace(pattern, "");
-        log.info(lang);
-        // var html  = "http://esprit.io/legal/"+lang+".privacy.html";
-        // var html = Meteor.call("getPpLink",lang);
-        Meteor.call("getPpLink", lang, function (error, result) {
-          if (error) {
-            log.error("error", error);
-          }
-          else {
-            return legalLink.set(result);
-          }
-        });
-        // return html;
-      },
-      function () {
-        alert('Error getting language\n');
-      }
-    );
-  }
-  else {
-    Meteor.call("getPpLink", "en", function (error, result) {
-      if (error) {
-        log.error("error", error);
-      }
-      else {
-        return legalLink.set(result);
-      }
-    });
-  }*/
+
 };
 
 
