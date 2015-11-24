78856b3cd30ebfdb9cbce232c81e8a3ce08eaaba
Terence Ng <terence@Terences-Mac-mini.local>
2015-11-24 10:49:47 +0800

    #110  set default office hours values in session. remove unused code

--- app/client/templates/chatoption/chatoption.js
+++ app/client/templates/chatoption/chatoption.js
@@ -8,67 +8,54 @@ Template.Chatoption.events({
   'change input': function (argument) {
     var insertDocObj = AutoForm.getFormValues("chatOptionUpdate").insertDoc;
     Session.set("optionObj", insertDocObj);
-  },
-
-  'change .timeCb': function () {
-    weeks = [];
-    $('.timeCb:checked').each(function () {
-      weeks.push($(this).val());
-    });
-    workHourTime.weeks = weeks;
-    // Session.set('workHourTime', workHourTime );
-    var optionObj = Session.get('optionObj');
-    //find out why optionObj.workHourTime is undefined
-    if (optionObj.workHourTime)
-      optionObj.workHourTime.weeks = weeks;
-    Session.set('optionObj', optionObj);
-  },
-
-  'change .from': function () {
-    var optionObj = Session.get('optionObj');
-    var from = $('.from').val();
-    if (optionObj.workHourTime)
-      optionObj.workHourTime.from = from;
-    Session.set('optionObj', optionObj);
-  },
-
-  'change .to': function () {
-    var optionObj = Session.get('optionObj');
-    var to = $('.to').val();
-    if (optionObj.workHourTime)
-      optionObj.workHourTime.to = to;
-    Session.set('optionObj', optionObj);
+    
   }
 });
 
 /* Chatoption: Created Handlers */
 Template.Chatoption.created = function () {
-  Session.setDefault("optionObj", {});
-  if (Meteor.user().profile.chatSetting) {
+  //clean up optionObj session 
+  Session.set("optionObj", {});
+  
+  //if user has set chatsetting before, retrieve it and set it to session
+  if (Meteor.user() && Meteor.user().profile && Meteor.user().profile.chatSetting) {
     var newOptionObj = lodash.assign(Meteor.user().profile.chatSetting, Session.get("optionObj"));
     Session.set("optionObj", newOptionObj);
   }
-  //new code from work_time_selection.js
+  
+  //get the latest session
   var optionObj = Session.get('optionObj');
   log.info(optionObj);
-  if (Meteor.user()["profile.chatSetting.workHourTime"]) {
+  
+  //append workHourTime setting to the session if user has set it before
+  if (Meteor.user() && Meteor.user()["profile.chatSetting.workHourTime"]) {
     optionObj.workHourTime = Meteor.user().profile.chatSetting.workHourTime;
     Session.set('optionObj', optionObj);
   }
-  else if (!optionObj.workHourTime) {
+  else if (!optionObj.workHourTime) { //else, define default value to the user via the session
     var workHourTime = {};
-    workHourTime.from = "";
-    workHourTime.to = "";
-    workHourTime.weeks = [];
+    workHourTime.from = "08:00";
+    workHourTime.to = "18:00";
+    workHourTime.weeks = {
+      mon:true,
+      tue:true,
+      wed:true,
+      thu:true,
+      fri:true,
+      sat:false,
+      sun:false
+    };
     optionObj.workHourTime = workHourTime;
     Session.set('optionObj', optionObj);
+   
   }
   else if (optionObj.workHourTime){
     log.info("optionObj.workHourTime set");
   }
   else{
     log.warn("optionObj.workHourTime NO set");
-  }
+  }   
+
 };
 
 /* Chatoption: Helpers */
@@ -78,6 +65,7 @@ Template.Chatoption.helpers({
   createdClassByMe: Classes.find(),
 
   optionObj: function (argument) {
+    log.info(Session.get('optionObj'));
     return Session.get('optionObj');
   },
 
@@ -87,28 +75,6 @@ Template.Chatoption.helpers({
       return lodash.includes(weeks, week) ? "checked" : "";
     }
     return "";
-  },
-
-  from: function (argument) {
-    var from = Session.get('optionObj').workHourTime.from;
-    if (from === "" || !from) {
-      var optionObj = Session.get('optionObj');
-      from = moment().format("09:00");
-      optionObj.workHourTime.from = from;
-      Session.set('optionObj', optionObj);
-    }
-    return from;
-  },
-
-  to: function (argument) {
-    var to = Session.get('optionObj').workHourTime.to;
-    if (to === "" || !to) {
-      var optionObj = Session.get('optionObj');
-      to = moment().format("17:00");
-      optionObj.workHourTime.to = to;
-      Session.set('optionObj', optionObj);
-    }
-    return to;
   },
 
   ishidden: function (argument) {
@@ -132,13 +98,10 @@ Template.Chatoption.helpers({
     if (weeks.length < 1) {
       return "";
     } else {
-      
-      
-
-       var weeksName = lodash.mapKeys(weeks, getWeekName);
+      var weeksName = lodash.mapKeys(weeks, getWeekName);
       log.info(weeksName);
-        var weeksNameFilter = lodash.pick(weeksName, lodash.identity);
-        log.info(weeksNameFilter);
+      var weeksNameFilter = lodash.pick(weeksName, lodash.identity);
+      log.info(weeksNameFilter);
       return lodash.keys(weeksNameFilter).toString();
     }
   }
@@ -147,11 +110,8 @@ Template.Chatoption.helpers({
 /*****************************************************************************/
 /* Chatoption: Lifecycle Hooks */
 /*****************************************************************************/
-
-
 Template.Chatoption.rendered = function () {
   
- 
 };
 
 Template.Chatoption.destroyed = function () {
@@ -161,30 +121,6 @@ Template.Chatoption.destroyed = function () {
 Template.ionNavBar.events({
   'click .ChatoptionSave': function (e, template) {
     AutoForm.submitFormById("#chatOptionUpdate");
-  },
-
-  'click .ChatoptionCancel': function (argument) {
-    var doc = Session.get("optionObj");
-    if (lodash.get(doc, "workHour")) {
-      var allWork = lodash.get(doc, "workHour") &&
-        lodash.get(doc, "workHourTime.from") &&
-        lodash.get(doc, "workHourTime.to") &&
-        lodash.get(doc, "workHourTime.weeks") &&
-        lodash.get(doc, "workHourTime.weeks").length > 0;
-
-      log.info(allWork);
-      if (allWork) {
-        AutoForm.submitFormById("#chatOptionUpdate");
-      }
-      else {
-        Session.set("optionObj", {});
-        window.history.back();
-      }
-    } else {
-      Session.set("optionObj", {});
-      AutoForm.submitFormById("#chatOptionUpdate");
-    }
-
   }
 });
 
