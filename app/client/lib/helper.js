1e909476c3d691c6ccb28927c4fb69ccb8e3a219
Terence Ng <terence@Terences-Mac-mini.local>
2015-11-27 12:07:17 +0800

    #132 comment for iconChooseHelper

--- app/client/lib/helper.js
+++ app/client/lib/helper.js
@@ -5,19 +5,24 @@ Template.registerHelper('iconChooseHelper',function(iconArray){
    if(iconArray){    
        var iconArrayLength = iconArray.length;   
        iconArray.forEach(function(currentValue,index){
+         //start a row for every COLUMN items
          if((index+1) % COLUMN == 1){
              output.push( "<div class='row'>" );
          }  
          output.push( "<div class='col'><a data-dismiss='modal'><i title='"+currentValue+"' class='icon e1a-"+currentValue+" e1a-5x emojicon'></i></a></div>" );
+         
+         //close a row for every COLUMN items
          if((index+1) % COLUMN == 0){     
              output.push( "</div>" );
          }
          
+         //at the end of the for each loop, fill in empty columns so the last row looks nice
          if((index+1) == iconArrayLength){
              var remainCols = (index+1) % COLUMN;     
              while(remainCols > 0){
                  output.push("<div class='col'></div>");
                  if(remainCols == 1){
+                     //close a row for the last item
                      output.push("</div>");
                  }
                  remainCols--;
