3d4458f0338a0046e3bc76ccec1c5641f2600ed2
Terence Ng <terenceng2010@gmail.com>
2015-12-14 12:06:29 +0800

    Love Little Genius send via email client on desktop #156

--- app/client/templates/TabsPage/tab_you/tab_you.js
+++ app/client/templates/TabsPage/tab_you/tab_you.js
@@ -10,27 +10,29 @@ Template.TabYou.events({
   },
 
   'click .love': function (argument) {
-    if (Meteor.isCordova) {
+      var text;
       switch (Meteor.user().profile.role) {
         //TODO localization English hardcoded for recommendation, with template i18n, see doc on how to insert variable
         case "Teacher":
-          var text = "Hey! \r\n\r\nI have been using Little Genius to text my students and parents without sharing my " +
+            text = "Hey! \r\n\r\nI have been using Little Genius to text my students and parents without sharing my " +
             "personal phone number.\r\nYou have to try it! It saves time, students love it and it is free! " +
             "\r\nHere is the link: " + Meteor.settings.public.SHARE_URL + "?rid=" + Meteor.userId();
           break;
         case "Student":
-          var text = "Hey!\r\n\r\nMy teachers have been using the Little Genius app to message our class before " +
+            text = "Hey!\r\n\r\nMy teachers have been using the Little Genius app to message our class before " +
             "assignments are due, share photos and update us with last minute changes. You should tell your teachers about it! " +
             "It\'s really helpful and free.\r\nHere is the link: " + Meteor.settings.public.SHARE_URL + "?rid=" + Meteor.userId() + "\"\r\n";
           break;
         case "Parent":
-          var text = "Hey!\r\n\r\nOur school teachers have been using the Little Genius app to message our class before " +
+            text = "Hey!\r\n\r\nOur school teachers have been using the Little Genius app to message our class before " +
             "assignments are due, share photos and update us with last minute changes. You should tell your school teachers about it! " +
             "It\'s really helpful and free.\r\nHere is the link: " + Meteor.settings.public.SHARE_URL + "?rid=" + Meteor.userId() + "\"\r\n";
           break;
 
         default:
       }
+      
+    if (Meteor.isCordova) {
       window.plugins.socialsharing.share(text, null, null, null);
       analytics.track("Referral", {
         date: new Date(),
@@ -42,6 +44,11 @@ Template.TabYou.events({
         }
       });
     }//end of isCordova
+    else{
+      //if is not cordova, try to send via user email client
+      window.location.href = "mailto:mail@example.org?body="+text;
+    }
+    
   }
 });
 
