788195d1f3e16f337e6921a08ec1d98eae570686
Terence Ng <terenceng2010@gmail.com>
2016-01-22 14:27:04 +0800

    bump app version

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -7,7 +7,7 @@ App.info({
   email: 'contact@littlegenius.io',
   website: 'https://app.gosmartix.com',
   //todo sync this with config.xml
-  version: '1.0.291111',
+  version: '1.0.292000',
   buildNumber: '101'
 });
 
