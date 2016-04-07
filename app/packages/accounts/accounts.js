86afd3a170305408c779659d1dd1326a4b2e4f0a
Terence Ng <terenceng2010@gmail.com>
2016-04-07 17:05:17 +0800

    add soft delete and edit user methods

---
+++ app/packages/accounts/accounts.js
@@ -0,0 +1,46 @@
+if(Meteor.isServer){
+    Meteor.methods({
+        'smartix:accounts/deleteUser':function(id){
+           var targetUser = Meteor.users.findOne(id);
+           
+            if(
+              Roles.userIsInRole(Meteor.userId(),'admin','system')
+             ){
+              
+              //Perform `update` operation using `alanning:roles`,
+              //removing the appropriate object from the `roles` array
+              //Roles.removeUsersFromRoles(user,['user'],'global')
+              
+              //Soft-delete user should not be done in school-level
+               Meteor.users.update({_id: user},  {$set: {deleted : true,deletedAt: (new Date()).getTime()}}  );
+               
+            }
+                      
+        },
+        'smartix:accounts/editUser':function(id,options,namespace){
+           var targetUser = Meteor.users.findOne(id);
+           if(!targetUser){
+               return;
+           }
+           
+           if(Meteor.userId() == id ||
+              Roles.userIsInRole(Meteor.userId(),'admin','system') ||
+              Roles.userIsInRole(Meteor.userId(),'admin',namespace)
+             )
+           {
+              //if it is normal user, cannot change self's role 
+              if(!Roles.userIsInRole(Meteor.userId(),'admin','system') && !Roles.userIsInRole(Meteor.userId(),'admin',namespace)){
+                 delete options.roles;
+              }
+              //console.log(options.doc);
+              
+              //`lodash.merge` would do a recursive operations to update the fields passed from `options`.
+              var ModifiedDoc = lodash.merge(targetUser, options);              
+              //console.log(ModifiedDoc);
+              Meteor.users.update({_id: id},  ModifiedDoc  );
+       
+           }            
+           
+        }        
+    });    
+}
