22a4ab1d080ee6d081d168fd24b68e192355095f
Terence Ng <terenceng2010@gmail.com>
2016-04-08 11:16:48 +0800

    can change background color and textcolor by setting sessions

--- app/packages/smartix-ui-master/master/master_layout.js
+++ app/packages/smartix-ui-master/master/master_layout.js
@@ -1,5 +1,31 @@
 /*! Copyright (c) 2015 Little Genius Education Ltd.  All Rights Reserved. */
-Template.MasterLayout.helpers({});
+Template.MasterLayout.helpers({
+    customizeTheme:function(){
+        
+        var schoolBackgroundColor = Session.get('schoolBackgroundColor')  ;
+        var schoolTextColor       = Session.get('schoolTextColor');     
+        if(schoolBackgroundColor && schoolTextColor){
+            var customStyle = `
+                                <style>                    
+                                    .bar.bar-stable,.button.button-stable,.tabs,.button-bar .button-bar-button,
+                                    .input-box-panel .button
+                                    {background-color:${schoolBackgroundColor};color:${schoolTextColor};}
+                                    
+                                    .bar.bar-stable .title{
+                                        color:${schoolTextColor};
+                                    }
+                                </style>
+            
+                            `;
+            
+            return customStyle;            
+        }else{
+            return "";
+        }
+
+    }
+    
+});
 
 Template.MasterLayout.events({});
 
