e31146528d7e2f86e67c3659b5ce42ac3417a635
Terence Ng <terenceng2010@gmail.com>
2016-04-11 10:56:04 +0800

    check if user has roles in pub/sub of schools

--- app/packages/schools/publications.js
+++ app/packages/schools/publications.js
@@ -1,8 +1,12 @@
-Meteor.publish('mySchools',function(){
-   var currentUserId = this.userId;
-   var currentUser = Meteor.users.findOne({_id: currentUserId});
-   var schoolIds = Object.keys(currentUser.roles); 
-   return SmartixSchoolsCol.find({_id:{$in: schoolIds} });
+Meteor.publish('mySchools', function() {
+    if (this.userId) {
+        var currentUserId = this.userId;
+        var currentUser = Meteor.users.findOne({ _id: currentUserId });
+        if (currentUser.roles) {
+            var schoolIds = Object.keys(currentUser.roles);
+            return SmartixSchoolsCol.find({ _id: { $in: schoolIds } });
+        }
+    }
 });
 
 Meteor.publish('allSchools',function(){
