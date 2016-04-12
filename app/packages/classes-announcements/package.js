bbecb6a19fdd44e48039f701915cdf8cbe6223b1
Terence Ng <terenceng2010@gmail.com>
2016-04-12 10:32:39 +0800

    publications should only run on server side

--- app/packages/classes-announcements/package.js
+++ app/packages/classes-announcements/package.js
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
   api.addFiles('classes-announcements.js');
-  api.addFiles('publications.js');
+  api.addFiles('publications.js','server');
 });
 
 Package.onTest(function(api) {
