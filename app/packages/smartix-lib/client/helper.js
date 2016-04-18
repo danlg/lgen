4bf70e5da2159b75c72a40e014f3b2ad5df3aa04
Terence Ng <terenceng2010@gmail.com>
2016-04-18 17:27:35 +0800

    support js date

--- app/packages/smartix-lib/client/helper.js
+++ app/packages/smartix-lib/client/helper.js
@@ -345,20 +345,47 @@ Template.registerHelper('formatTime', function(time) {
 
 Template.registerHelper('formatDate', function(time) {
     var dateString="";
-    if(time){  
-      //log.info(this);
-      var fullUnixTime = time;
-      if (fullUnixTime){
-        var trimUnixTime = fullUnixTime.substr(0,10);
-        var userLanguage = TAPi18n.getLanguage();
-        moment.locale(userLanguage);
+    if(time){
         
-        dateString = moment.unix(trimUnixTime).format('LL');
-      }       
+      if( moment( new Date(time)).isValid() ){
+           dateString = moment(time).format('h:mm a');
+      }else{
+        //log.info(this);
+        var fullUnixTime = time;
+        if (fullUnixTime){
+            var trimUnixTime = fullUnixTime.substr(0,10);
+            var userLanguage = TAPi18n.getLanguage();
+            moment.locale(userLanguage);
+            
+            dateString = moment.unix(trimUnixTime).format('LL');
+        }             
+      }        
+    
     }
     return dateString;
 });
 
+Template.registerHelper('formatDate', function(time) {
+    var dateString="";
+    if(time){
+        
+      if( moment( new Date(time)).isValid() ){
+           dateString = moment(time).format('LL');
+      }else{
+        //log.info(this);
+        var fullUnixTime = time;
+        if (fullUnixTime){
+            var trimUnixTime = fullUnixTime.substr(0,10);
+            var userLanguage = TAPi18n.getLanguage();
+            moment.locale(userLanguage);
+            
+            dateString = moment.unix(trimUnixTime).format('LL');
+        }             
+      }        
+    
+    }
+    return dateString;
+});
 
 
 function ping(ip, callback) {
