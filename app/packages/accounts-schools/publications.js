bfc9fb7f7e32505e792d4a167e02a86fff3a4904
Terence Ng <terenceng2010@gmail.com>
2016-04-20 15:57:59 +0800

    add pub allSchoolUsersPerRole

--- app/packages/accounts-schools/publications.js
+++ app/packages/accounts-schools/publications.js
@@ -21,6 +21,70 @@ Meteor.publish('allSchoolUsers', function (school) {
     }
 });
 
+Meteor.publish('allSchoolUsersPerRole', function (school) {
+    // Get the `_id` of the school from its username
+    var schoolDoc = SmartixSchoolsCol.findOne({
+        username: school
+    });
+    
+    var allSchoolUsers = [];    
+    //if user is a teacher:
+    //can talk to another teacher
+    //can talk to students who the teacher is teaching
+    //can talk to parents whose students are taught by the teacher
+    //can talk to students who the teacher is not teaching
+    if (Roles.userIsInRole(this.userId, 'teacher', schoolDoc._id)) {
+
+        var teachers = Roles.getUsersInRole('teacher', schoolDoc._id).fetch();
+        var students = Roles.getUsersInRole('student', schoolDoc._id).fetch();
+
+        var studentsWhoTaughtByTeacher = Smartix.Groups.Collection.find({ namespace: schoolDoc._id, admins: this.userId }).fetch();
+
+        var parents = [];
+        lodash.map(studentsWhoTaughtByTeacher, 'users').map(function (studentIDs) {
+
+            studentIDs.map(function (studentID) {
+                //console.log('studentID', studentID);
+                var findParents = Relationships.find({ child: studentID, namespace: schoolDoc._id }).fetch();
+                //console.log('findParents', findParents);
+                findParents.map(function (relationship) {
+                    parents.push(relationship.parent);
+                });
+            });
+        });      
+       
+        allSchoolUsers = allSchoolUsers.concat( lodash.map(teachers,'_id') );
+        allSchoolUsers = allSchoolUsers.concat( lodash.map(students,'_id') );
+        allSchoolUsers = allSchoolUsers.concat( parents );
+        //var parents = Roles.getUsersInRole('parent',schoolDoc._id).fetch();  
+        
+        console.log('allSchoolUsersForTeacher',allSchoolUsers);
+
+        //if user is a parent:
+        //can talk to another parent
+        //can talk to parent's own student
+        //can talk to teacher, who teach to parent's own student
+    } else if (Roles.userIsInRole(this.userId, 'parent', schoolDoc._id)) {
+        //TODO
+
+        //if user is a student:
+        //can talk to teacher who teach the student
+        //can talk to student's own parent
+    } else if (Roles.userIsInRole(this.userId, 'student', schoolDoc._id)) {
+        //TODO
+
+    }
+
+    
+    if(schoolDoc) {
+        return Meteor.users.find(
+            { _id: { $in: allSchoolUsers } }
+        );
+    } else {
+        this.ready();
+    }
+});
+
 Meteor.publish('schoolUser', function (school, userId) {
     // Check if the user has permission for this school
     Smartix.Accounts.isUserSchoolAdmin(school, this.userId);
