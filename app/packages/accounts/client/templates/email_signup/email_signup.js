497411ae7125b612b930331ddf269848c26b08f5
Daniel Li <dan@danyll.com>
2016-04-21 14:29:13 +0800

    Remove the template `role`, as users do not have to choose role with new structure

--- app/packages/accounts/client/templates/email_signup/email_signup.js
+++ app/packages/accounts/client/templates/email_signup/email_signup.js
@@ -19,9 +19,6 @@ Template.EmailSignup.helpers({
   emailSignup: function (argument) {
     Schema.emailSignup.i18n("schemas.emailSignup");
     return Schema.emailSignup;
-  },
-  isStudent: function () {
-    return Template.instance().chosenRole.get() === "Student";
   }
 
 });
@@ -32,11 +29,8 @@ Template.EmailSignup.helpers({
 Template.EmailSignup.created = function () {
   var classToBeJoined = Session.get("search");
   console.log(classToBeJoined);
-  console.log("chosen role: " + Router.current().params.role);
 
   $("body").removeClass('modal-open');
-  this.chosenRole = new ReactiveVar('');
-  console.log("chosen role: ",this.chosenRole.get());
 };
 
 Template.EmailSignup.rendered = function () {
@@ -48,31 +42,11 @@ Template.EmailSignup.destroyed = function () {
 };
 
 Template.EmailSignup.events({
-  'click .role-selection':function(event,template){
-      //$(event.target).data('role');
-      $(".role-selection").removeClass('chosen-role');
-      $(event.currentTarget).addClass('chosen-role');          
-      template.chosenRole.set($(event.currentTarget).data('role'));
-  },
-  'change #roleOptions':function(event,template){
-      template.chosenRole.set($("#roleOptions").val());
-      console.log("chosen role: ",template.chosenRole.get());
-  },
-  'click .createBtn': function (event,template) {
-
-
-    // AutoForm.submitFormById("#signupform");
-    var role = template.chosenRole.get();
-    if(role == ""){
-        toastr.info('Tell us whether you are a teacher, student or parent!');
-        return;
-    }
     var userObj = {};
     userObj.profile = {};
     userObj.email = $(".email").val();
     userObj.profile.firstName = $(".fn").val();
     userObj.profile.lastName = $(".ln").val();
-    userObj.profile.role = role;
     userObj.dob = $("#dobInput").val() || "";
 
     //if () {
