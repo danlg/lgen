df43ff7130f509e83e2bfa264023d7187d4bec38
Daniel Li <dan@danyll.com>
2016-04-22 16:14:03 +0800

    Exposed `Smartix.Messages.Addons.Poll.Schema` to client

--- app/packages/messages-addons-poll/server/messages-addons-poll.js
+++ app/packages/messages-addons-poll/server/messages-addons-poll.js
@@ -8,37 +8,7 @@ Smartix.Messages.Addons = Smartix.Messages.Addons || {};
 
 Smartix.Messages.Addons.ValidTypes = Smartix.Messages.Addons.ValidTypes || [];
 
-Smartix.Messages.Addons.Poll = {};
-
-Smartix.Messages.Addons.Poll.Type = 'poll';
-
-Smartix.Messages.Addons.ValidTypes = _.union(Smartix.Messages.Addons.ValidTypes, [Smartix.Messages.Addons.Poll.Type]);
-
-Smartix.Messages.Addons.Poll.Schema = new SimpleSchema({
-    type:{
-        type:String
-    },    
-    options: {
-        type: [String]
-    },
-    votes: {
-        type: [Object]
-    },
-    "votes.$.option": {
-        type: String
-    },
-    "votes.$.users": {
-        type: [String]
-    },
-    multiple: {
-        type: Boolean,
-        defaultValue: false
-    },
-    expires: {
-        type: Number,
-        optional: true
-    }
-});
+Smartix.Messages.Addons.Poll = Smartix.Messages.Addons.Poll || {};
 
 Smartix.Messages.Addons.Poll.canChangeVotes = function (messageId) {
     // Retrieve information about the message
