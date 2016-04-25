af8830d64705d63c21149d59b41843042505cb1e
Aman <amangupta96@outlook.com>
2016-04-25 18:17:39 +0800

    Added SMTP Injection Variables

--- app/server/methods.js
+++ app/server/methods.js
@@ -418,4 +418,13 @@ Meteor.methods({
   }
 
 });
+Meteor.startup(function () {
 
+  process.env.MAIL_URL = 'smtp://' + 
+      encodeURIComponent(Meteor.settings.smtp.username) + ':' + 
+      encodeURIComponent(Meteor.settings.smtp.password) + '@' +
+      encodeURIComponent(Meteor.settings.smtp.server) + ':' +
+      Meteor.settings.smtp.port;
+      
+ console.log(process.env.MAIL_URL);
+});
