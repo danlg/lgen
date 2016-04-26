0370c008fd5dfc96574b6f7186c5961c318c6208
Terence Ng <terenceng2010@gmail.com>
2016-04-26 21:49:53 +0800

    display vote icon with display vote

--- app/packages/messages-addons-poll/lib/messages-addons-poll.js
+++ app/packages/messages-addons-poll/lib/messages-addons-poll.js
@@ -13,6 +13,19 @@ Smartix.Messages.Addons.ValidTypes = Smartix.Messages.Addons.ValidTypes || [];
 
 Smartix.Messages.Addons.ValidTypes = _.union(Smartix.Messages.Addons.ValidTypes, [Smartix.Messages.Addons.Poll.Type]);
 
+/**
+ * Represents a Poll.
+ * @constructor
+ * @param {string} type - type of addons. in this case, it is poll
+ * @param {string array} options - String Array that stores what options are available in the poll. This field is useful for doing query
+ * @param {object array} votes - Object Array that stores options detail, consists for below four property
+ * @param {String} votes.$.option" - store a single option name
+ * @param {String array} votes.$.users - string array that stores user's id if they have polled
+ * @param {String} votes.$.optionIconType - show type of icon used. e.g icon-emojicon, icon-ionicon , image, text
+ * @param {String} votes.$.optionIconValue - related value of optionIconType, e.g if optionIconType is image, imageId should be stored in this field. if optionIconType is text, actual text to be displayed should be stored in this field.
+ * @param {Boolean} multiple - whether user can vote for multiple options - not yet implement.  
+ * @param {Number} expires - time that the vote expires - not yet implement.        
+ */
 Smartix.Messages.Addons.Poll.Schema = new SimpleSchema({
     type:{
         type:String
@@ -26,9 +39,20 @@ Smartix.Messages.Addons.Poll.Schema = new SimpleSchema({
     "votes.$.option": {
         type: String
     },
+    "votes.$.optionShowEvenNoVote": {
+        type: Boolean,
+        defaultValue:true
+    },       
     "votes.$.users": {
         type: [String]
     },
+    "votes.$.optionIconType": {
+        type: String
+    },
+    "votes.$.optionIconValue": {
+        type: String
+    },  
+        
     multiple: {
         type: Boolean,
         defaultValue: false
