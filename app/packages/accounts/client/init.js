4b5f8e928e8b24f5e7e992bdb24eccf2b5603814
Terence Ng <terenceng2010@gmail.com>
2016-04-27 15:07:32 +0800

    This function should be called in top-level code, not inside Meteor.startup().

--- app/packages/accounts/client/init.js
+++ app/packages/accounts/client/init.js
@@ -1,12 +1,9 @@
-Meteor.startup(function () {
-    
-    Accounts.onResetPasswordLink(function(token, done) {
-        Session.set('resetPasswordToken', token);
-        Router.go('EmailResetPwd');
-    });
+Accounts.onResetPasswordLink(function(token, done) {
+    Session.set('resetPasswordToken', token);
+    Router.go('EmailResetPwd');
+});
 
-    Accounts.onEnrollmentLink(function(token, done) {
-        Session.set('resetPasswordToken', token);
-        Router.go('EmailResetPwd');
-    });
+Accounts.onEnrollmentLink(function(token, done) {
+    Session.set('resetPasswordToken', token);
+    Router.go('EmailResetPwd');
 });
