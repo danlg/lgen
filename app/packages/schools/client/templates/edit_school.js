7eeb2c69c657e7f168daa4ba7e941a52d4167259
Terence Ng <terenceng2010@gmail.com>
2016-04-29 12:50:47 +0800

    better tips in edit school

--- app/packages/schools/client/templates/edit_school.js
+++ app/packages/schools/client/templates/edit_school.js
@@ -38,8 +38,11 @@ Template.EditSchool.events({
         
         Meteor.call('smartix:schools/createSchool',newSchoolObj,function(err,result){
             if(err){
+                toastr.error(err.reason);
                 console.log(err);
             }else{
+                toastr.info('create school success: you may sign out from system admin to continue');
+                toastr.info('initial admin:' + result.initialAdmin.username +' pw:' + result.initialAdmin.initialPassword);
                 console.log(result);
             }
         } );
