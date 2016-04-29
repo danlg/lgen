43e79c68aba167d341364208b374c788ca997a56
Terence Ng <terenceng2010@gmail.com>
2016-04-29 10:43:36 +0800

    revert to previous notifications pub

--- app/packages/smartix-notifications/publications.js
+++ app/packages/smartix-notifications/publications.js
@@ -1,23 +1,7 @@
 /*! Copyright (c) 2015 Little Genius Education Ltd.  All Rights Reserved. */
 
-Meteor.publish('notifications', function (namespace) {
-  //log.info("publish:notificaitons:"+ this.userId);
-  
-  if(namespace){
-  var groupsInNamespace = Smartix.Groups.Collection.find({namespace:namespace},{fields: {_id:1}}).fetch();
-  var groupIdsInNamespace = lodash.map(groupsInNamespace,'_id');
-  
-    return Notifications.find({
-      userId: this.userId,
-      groupId : { $in : groupIdsInNamespace}
-    });          
-  }else{
-    
+Meteor.publish('notifications', function () {
     return Notifications.find({
       userId: this.userId
     });    
-  }
-
-  
-
 });
