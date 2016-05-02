7b6c8cda3d338486b5f07e03cb68085ae24047ec
Terence Ng <terenceng2010@gmail.com>
2016-05-02 17:53:48 +0800

    move tap-i18n to smartix:lib package so that it will still work under 1.3.2.4

--- app/packages/smartix-core/package.js
+++ app/packages/smartix-core/package.js
@@ -9,11 +9,10 @@ Npm.depends ({
 });
 
 Package.onUse(function(api) {
-	  api.versionsFrom('1.2');
+	api.versionsFrom('1.2');
     api.use('ecmascript');
     api.use('practicalmeteor:loglevel');
     api.use('iron:router');
-    api.use('tap:i18n');
     api.use('reactive-var','client');
     api.use('session', 'client');    
     api.use('templating', 'client');
@@ -62,23 +61,7 @@ Package.onUse(function(api) {
                  ],
                 'client');
                                                                                                   
-    //lanaguage-specific app tour image
-    api.addFiles([
-                    'i18n/fr/app_tour_image.fr.scss',
-                    'i18n/zh-CN/app_tour_image.zh-CN.scss',
-                    'i18n/zh-TW/app_tour_image.zh-TW.scss',
-                 ],'client');
-    
-    // TAPi18n
-    api.addFiles([
-                    'i18n/en/en.i18n.json',
-                    'i18n/fr/fr.i18n.json',
-                    'i18n/zh-CN/zh-CN.i18n.json',
-                    'i18n/zh-TW/zh-TW.i18n.json',
-                    'i18n/project-tap.i18n'           
-                 ]);
-	api.use('tap:i18n');
-	api.imply('tap:i18n');
+
     
     
     //initialize dependency
--- app/packages/smartix-lib/package.js
+++ app/packages/smartix-lib/package.js
@@ -12,11 +12,31 @@ Package.onUse(function(api) {
     api.use('templating','client');
     api.use('stevezhu:lodash');
     api.use('smartix:classes');
+    api.use('fourseven:scss@2.0.0','client');    
+ 
     api.addFiles('client/helper.js','client');
     api.addFiles('shared/helper.js');
     api.addFiles('shared/find-in-json.js');
     api.addFiles('shared/method_cs.js');
     api.addFiles('server/methods.js','server');
+
+    //lanaguage-specific app tour image
+    api.addFiles([
+                    'i18n/fr/app_tour_image.fr.scss',
+                    'i18n/zh-CN/app_tour_image.zh-CN.scss',
+                    'i18n/zh-TW/app_tour_image.zh-TW.scss',
+                 ],'client');
     
+    // TAPi18n
+    api.addFiles([
+                    'i18n/en/en.i18n.json',
+                    'i18n/fr/fr.i18n.json',
+                    'i18n/zh-CN/zh-CN.i18n.json',
+                    'i18n/zh-TW/zh-TW.i18n.json',
+                    'i18n/project-tap.i18n'           
+                 ]);
+	api.use('tap:i18n');
+	api.imply('tap:i18n');
+        
     api.export('Smartix');
 });
