ed147a3059b7289b95408b94b6328ede1d380b50
Daniel Li <dan@danyll.com>
2016-05-03 15:16:59 +0800

    Dynamically listing out the newsgroups. Resolves #320.

--- app/packages/admin/client/templates/news/add/add.js
+++ app/packages/admin/client/templates/news/add/add.js
@@ -1,3 +1,42 @@
+Template.AdminNewsAdd.onCreated(function () {
+    this.autorun(() => {
+        this.currentSchoolName = new ReactiveVar('currentSchoolName');
+        if(Router
+            && Router.current()
+            && Router.current().params
+            && Router.current().params.school
+        ) {
+            this.currentSchoolName = Router.current().params.school;
+            this.subscribe('schoolInfo', this.currentSchoolName, (err, res) => {
+                if(!err) {
+                    this.subscribe('smartix:newsgroups/allNewsgroupsFromSchoolName', this.currentSchoolName, function (err, res) {
+                        // console.log(err);
+                        // console.log(res);
+                    });
+                }
+            });
+        }
+        
+    })
+    
+});
+
+Template.AdminNewsAdd.helpers({
+    newsgroups: function () {
+        if(Template.instance().subscriptionsReady()) {
+            var schoolDoc = SmartixSchoolsCol.findOne({
+                username: Template.instance().currentSchoolName
+            });
+            if(schoolDoc) {
+                return Smartix.Groups.Collection.find({
+                    namespace: schoolDoc._id,
+                    type: 'newsgroup'
+                });
+            }
+        }
+    }
+})
+
 Template.AdminNewsAdd.events({
     'click #addNews-submit': function (event, template) {
 
@@ -7,8 +46,6 @@ Template.AdminNewsAdd.events({
 
         $("input[type='checkbox'][name='addNews-newsgroup']").each(function (index) {
             if (this.checked) {
-                console.log(this.value); //where value equal newsgroup url
-
                 Meteor.call('smartix:messages/createNewsMessage', this.value, 'article', { content: content, title: title });
             }
         });
