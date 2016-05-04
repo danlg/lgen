481e12757d7d577f25bbbfd302a9b96ed0f349b8
Terence Ng <terenceng2010@gmail.com>
2016-05-05 01:23:40 +0800

    disable reactivity on sliding news since it is very hard to implement

--- app/packages/schools-home/templates/overview/overview.js
+++ app/packages/schools-home/templates/overview/overview.js
@@ -33,49 +33,67 @@ Template.MobileSchoolHome.helpers({
         return Router.current().params.school;
     },
     getSlidingNews:function(){
+        var currrenTemplate = Template.instance();
         var newsgroups =  Smartix.Groups.Collection.find({ type: 'newsgroup' }).fetch();
         //console.log('getSlidingNews:',newsgroups);
-        var newsgroupsIds = lodash.map(newsgroups,'_id');        
-        var messages = Smartix.Messages.Collection.find({ group: { $in: newsgroupsIds } }, {sort: {createdAt: -1 } } );        
+        var newsgroupsIds = lodash.map(newsgroups,'_id');
+        
+      
+            
+         var query = Smartix.Messages.Collection.find({ group: { $in: newsgroupsIds } }, {sort: {createdAt: -1 }, reactive:false } );        
         //console.log('getSlidingNews:', messages);
-        if(messages.count()>0){
-            Template.instance().slidingNewsLoaded.set(true);
-        }
-        return messages;
+               
+        
+        
+        return query;           
+                
+
+          
+
+
+
+
     }
     
     
 });
 
 Template.MobileSchoolHome.onCreated(function(){
+   
+   this.canGetSlidNews = new ReactiveVar(false);
    var self = this;
    self.subscribe('newsgroupsForUser',null,null, Router.current().params.school,function(){
-    self.subscribe('newsForUser',null,null, Router.current().params.school );       
+    self.subscribe('newsForUser',null,null, Router.current().params.school);       
    });
-   
-   this.slidingNewsLoaded = new ReactiveVar(false);
+     
 
 })
 
 Template.MobileSchoolHome.onRendered(function(){
+    var self = this;
 
- var self = this;
- self.autorun(function(){    
-  if(self.slidingNewsLoaded.get()){
-    self.$('.ion-slide-box').slick({
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        arrows: false,
-        dots: false,
-        dotsClass: 'slider-pager',
-        initialSlide: 0,
-        customPaging: function(slider, i) {
-        return '<span class="slider-pager-page icon ion-record"></span>';
-        }
-    });      
-  }     
- })
+    Meteor.setTimeout(function(){
+        
+        self.$('.ion-slide-box').slick({
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            arrows: false,
+            dots: false,
+            dotsClass: 'slider-pager',
+            initialSlide: 0,
+            customPaging: function(slider, i) {
+            return '<span class="slider-pager-page icon ion-record"></span>';
+            }
+        });
+    },1000);
+
+
+
+
+})
+
+Template.MobileSchoolHome.onDestroyed(function(){   
 
 
 })
