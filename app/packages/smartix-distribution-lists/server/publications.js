eee83af60efd65ad2c417cf0f60c0d0b8d781466
Daniel Li <dev@brew.com.hk>
2016-05-04 14:43:59 +0800

    Allow publishing of distribution lists by school name

--- app/packages/smartix-distribution-lists/server/publications.js
+++ app/packages/smartix-distribution-lists/server/publications.js
@@ -1,4 +1,23 @@
 Meteor.publish({
+    'smartix:distribution-lists/listsBySchoolName': function (schoolName) {
+        
+        check(schoolName, String);
+        
+        let namespace = Smartix.Accounts.School.getNamespaceFromSchoolName(schoolName);
+        
+        if(!namespace) {
+            throw new Meteor.Error("school-not-exist", "The school with code " + schoolName + " does not exist.")
+        }
+        
+        if(!Smartix.DistributionLists.hasPermission(namespace, this.userId)) {
+            throw new Meteor.Error("permission-denied", "The user does not have permission to perform this action.");
+        }
+        
+        return Smartix.Groups.Collection.find({
+            namespace: namespace,
+            type: "distributionList"
+        });
+    },
     'smartix:distribution-lists/listsInNamespace': function (namespace) {
         
         check(namespace, String);
