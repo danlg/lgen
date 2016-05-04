01c085cc8eaeee8184469ae30be68ff14427a0f0
Terence Ng <terenceng2010@gmail.com>
2016-05-04 16:49:10 +0800

    make the upload button clickable

--- app/packages/admin/client/templates/news/add/add.js
+++ app/packages/admin/client/templates/news/add/add.js
@@ -19,6 +19,10 @@ Template.AdminNewsAdd.onCreated(function () {
         
     })
     
+    this.imageArr = new ReactiveVar([]);
+    this.documentArr = new ReactiveVar([]);
+    this.calendarEvent = new ReactiveVar({});
+
 });
 
 Template.AdminNewsAdd.helpers({
@@ -51,5 +55,64 @@ Template.AdminNewsAdd.events({
                 Meteor.call('smartix:messages/createNewsMessage', this.value, 'article', { content: content, title: title }, null,doPushNotification);
             }
         });
-    }
+    },
+    'change #imageBtn': function (event, template) {
+        //https://github.com/CollectionFS/Meteor-CollectionFS
+        //Image is inserted from here via FS.Utility
+        Smartix.FileHandler.imageUpload(event,'class',template.imageArr.get(),
+            function(result){
+                template.imageArr.set(result);
+            });
+        showPreview("image");
+    },
+    'click .cancel-calendar':function(event,template){
+        template.calendarEvent.set({});
+    },
+    'click .set-calendar':function(event,sendMsgtemplate){
+        IonPopup.show({
+        title: 'Set a calendar event',
+        templateName: 'CalendarEvent',
+        buttons: [{
+            text: 'Set',
+            type: 'button-positive',
+            onTap: function(event,template) {
+            
+            log.info($(template.firstNode).find('#event-name').val());
+            
+            // $(template.firstNode).find('.hidden').click();
+            if($(template.firstNode).find('#event-name').get(0).checkValidity() &&
+            $(template.firstNode).find('#location').get(0).checkValidity() &&
+            $(template.firstNode).find('#start-date').get(0).checkValidity() &&
+            $(template.firstNode).find('#start-date-time').get(0).checkValidity() &&
+            $(template.firstNode).find('#end-date').get(0).checkValidity() &&
+            $(template.firstNode).find('#end-date-time').get(0).checkValidity())
+            { } else{
+                toastr.info('Please fill the form');
+                return;
+            }
+            
+            sendMsgtemplate.calendarEvent.set({
+                eventName: $(template.firstNode).find('#event-name').val(),
+                location: $(template.firstNode).find('#location').val(),
+                startDate:$(template.firstNode).find('#start-date').val(),
+                startDateTime:$(template.firstNode).find('#start-date-time').val(),
+                endDate:$(template.firstNode).find('#end-date').val(),
+                endDateTime:$(template.firstNode).find('#end-date-time').val()       
+            });
+            
+            log.info(sendMsgtemplate.calendarEvent.get());
+            
+            IonPopup.close();
+            }
+        }]
+        });
+    },
+    'click #documentBtn': function (event, template) {
+        Smartix.FileHandler.documentUpload(event,'class',template.documentArr.get(),function(result){
+            if(result){
+                template.documentArr.set([]);
+                window.setTimeout(scrollMessageListToBottom, 100);
+            }
+        });
+    }     
 });
