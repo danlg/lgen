0062fc2179fe059331bdc153202fdecdb1a38ecd
Terence Ng <terenceng2010@gmail.com>
2016-05-05 23:41:36 +0800

    basic implementation of calendar view of events #328

---
+++ app/packages/smartix-calendarevent/client/templates/list/list.js
@@ -0,0 +1,36 @@
+//TODO:
+
+//1. get all messages that have calendar addons that user is in the group
+//db.getCollection('smartix:messages').find({'addons.type':'calendar',group:{ $in: usergroups}})
+
+//2. display them like the list view shown in github
+
+Template.CalendarListView.onCreated(function(){
+   
+   var self = this;
+   self.subscribe('newsgroupsForUser',null,null,Session.get('pickedSchoolId'),function(){
+      self.subscribe('newsForUser',null,null,Session.get('pickedSchoolId'));
+   });    
+
+});
+
+Template.CalendarListView.helpers({
+    
+    getEvents:function(){
+        var newsgroups =  Smartix.Groups.Collection.find({ type: 'newsgroup', users: Meteor.userId() }).fetch(); 
+        var newsgroupsIds = lodash.map(newsgroups,'_id');
+        
+        //sort by event startDate in ascending order. Older events is displayed first.
+        //TODO. only showing incoming / ongoing event, filter out past event
+        return Smartix.Messages.Collection.find({'addons.type':'calendar', group: { $in: newsgroupsIds } }, {sort: {'addons.calendar.startDate': 1 } } );
+    },
+    getGroupName:function(groupId){
+        console.log('getGroupName',groupId);
+       return Smartix.Groups.Collection.findOne(groupId).name;
+    },
+    getCalendar:function(){
+        var calendarObjs =lodash.filter(this.addons, function(addon) { return addon.type =='calendar'; });
+        return calendarObjs[0];       
+    },
+    
+});
