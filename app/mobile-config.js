fcb540bef03e572350bddb8fc504715fefe89fd6
Daniel Rosenberg <dan@littlegenius.io>
2016-05-10 11:58:55 +0800

    set build 121 for carmel RC

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -8,7 +8,7 @@ App.info({
   email: 'contact@gosmartix.com',
   website: 'https://app.gosmartix.com',
   version: '1.2.0',
-  buildNumber: '120'
+  buildNumber: '121'
 });
 
 //generated with meteor-assets
