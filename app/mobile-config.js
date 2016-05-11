4c56156bc3bb3a7e8fc5f13d834ac12cd9cc6282
Daniel Rosenberg <dan@littlegenius.io>
2016-05-11 12:10:12 +0800

    bump build number 125

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -8,7 +8,7 @@ App.info({
   email: 'contact@gosmartix.com',
   website: 'https://app.gosmartix.com',
   version: '1.2.0',
-  buildNumber: '121'
+  buildNumber: '125'
 });
 
 //generated with meteor-assets
