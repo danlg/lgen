d7858a8d76e96e85253820bbf0ae729f41e1a36f
Terence Ng <terenceng2010@gmail.com>
2016-05-11 16:57:18 +0800

    add stub for processed attendance record

--- app/packages/absence/client/templates/home/home.js
+++ app/packages/absence/client/templates/home/home.js
@@ -1,5 +1,34 @@
 Template.AttendanceHome.helpers({
     getCurrentSchoolName: function() {
         return Router.current().params.school;
-    },    
-})
+    },
+    attendanceRecordProcessedRequests:function(){
+       var schoolDoc = SmartixSchoolsCol.findOne({
+           username: Router.current().params.school
+       });               
+       
+       return Smartix.Absence.Collections.processed.find({namespace:schoolDoc._id,status:'missing'});        
+    }
+});
+
+Template.AttendanceHome.onCreated(function(){
+    var self = this;
+    
+    self.subscribe('userRelationships', Meteor.userId());
+    self.subscribe('mySchools',function(){
+       var schoolDoc = SmartixSchoolsCol.findOne({
+           username: Router.current().params.school
+       });        
+      self.subscribe('smartix:absence/parentGetChildProcessed',schoolDoc._id); 
+    });    
+    self.subscribe('allSchoolUsersPerRole',Router.current().params.school);
+        
+});
+
+Template.AttendanceHome.events({
+   
+   'click .i-dont-know-btn':function(event,template){
+     var processId = $(event.target).data('id');  
+     //TODO: do something
+    } 
+});
