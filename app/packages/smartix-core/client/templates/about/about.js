773d08a6ad6b8be046a28debc0449f3ee0b96652
Daniel Rosenberg <dan@littlegenius.io>
2016-05-18 10:46:15 +0800

    version number hardcoded for now - we need to find a better way

--- app/packages/smartix-core/client/templates/about/about.js
+++ app/packages/smartix-core/client/templates/about/about.js
@@ -8,13 +8,16 @@ Template.About.helpers({
     //http://eclipsesource.com/blogs/2015/04/07/an-apache-cordova-hook-to-auto-bump-ios-cfbundleversion-and-android-versioncode/
     //https://gist.github.com/hstaudacher/d78b154509e2783cfcc2 - timestamp_version_bump.js
     if (Meteor.isCordova) {
-      version = window.cordova.compileTime.version;
-      //buildNumber = window.cordova.compileTime.buildNumber;
-      //return version + " build " + buildNumber;
+      //version = window.cordova.compileTime.version; //now broken with carmel branch
+      version = "1.2.1";
       return version;
     }
     //TODO fix me on web version !
-    else {return (Meteor.settings && Meteor.settings.VERSION) ?  Meteor.settings.VERSION : "1.0"}
+    else {
+      //let txt = Assets.getText("../../mobile-config.js");
+      log.info(txt);
+      return (Meteor.settings && Meteor.settings.VERSION) ?  Meteor.settings.VERSION : "1.2.1"
+    }
 
   },
 
