fddda23f20e02167f91f92bee70aafc912b47159
Daniel Rosenberg <dan@littlegenius.io>
2016-05-19 00:49:24 +0800

    bump version 160

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -8,7 +8,7 @@ App.info({
   email: 'contact@gosmartix.com',
   website: 'https://app.gosmartix.com',
   version: '1.2.1',
-  buildNumber: '153'
+  buildNumber: '160'
 });
 
 //generated with meteor-assets
