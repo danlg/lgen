b3e6e0fc93d8e9dec421cf5c205733153035a113
Terence Ng <terenceng2010@gmail.com>
2016-05-19 23:16:43 +0800

    move app tour images from base64 to files

--- app/packages/smartix-lib/package.js
+++ app/packages/smartix-lib/package.js
@@ -1,42 +1,62 @@
 Package.describe({
-  name: "smartix:lib",
-  summary: "",
-  version: "0.0.1",
-  documentation:'README.md'
+    name: "smartix:lib",
+    summary: "",
+    version: "0.0.1",
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom("1.2");
     api.use('iron:router');
     api.use('smartix:core');
-    api.use('templating','client');
+    api.use('templating', 'client');
     api.use('stevezhu:lodash');
-    api.use('smartix:classes', {unordered: true});
-    api.use('fourseven:scss@2.0.0','client');    
- 
-    api.addFiles('client/helper.js','client');
+    api.use('smartix:classes', { unordered: true });
+    api.use('fourseven:scss@2.0.0', 'client');
+
+    api.addFiles('client/helper.js', 'client');
     api.addFiles('shared/helper.js');
     api.addFiles('shared/find-in-json.js');
-    api.addFiles('server/methods.js','server');
+    api.addFiles('server/methods.js', 'server');
     api.addFiles('server/method_cs.js', 'server');
 
     //lanaguage-specific app tour image
-    api.addFiles([
-                    'i18n/fr/app_tour_image.fr.scss',
-                    'i18n/zh-CN/app_tour_image.zh-CN.scss',
-                    'i18n/zh-TW/app_tour_image.zh-TW.scss',
-                 ],'client');
+
+    api.addAssets([
+        'i18n/fr/fr_t1.jpg',
+        'i18n/fr/fr_t2.jpg',
+        'i18n/fr/fr_t3.jpg',
+        'i18n/fr/fr_t4.jpg',
+        'i18n/fr/fr_t5.jpg',
+        'i18n/fr/fr_t6.jpg',               
+    ], 'client');
     
+    api.addAssets([
+        'i18n/zh-CN/zh_cn_t1.jpg',
+        'i18n/zh-CN/zh_cn_t2.jpg',
+        'i18n/zh-CN/zh_cn_t3.jpg',
+        'i18n/zh-CN/zh_cn_t4.jpg',
+        'i18n/zh-CN/zh_cn_t5.jpg',
+        'i18n/zh-CN/zh_cn_t6.jpg',                              
+    ], 'client');
+           
+
+    api.addFiles([
+        'i18n/fr/app_tour_image.fr.scss',
+        'i18n/zh-CN/app_tour_image.zh-CN.scss',
+        'i18n/zh-TW/app_tour_image.zh-TW.scss',
+    ], 'client');
+
     // TAPi18n
     api.addFiles([
-                    'i18n/en/en.i18n.json',
-                    'i18n/fr/fr.i18n.json',
-                    'i18n/zh-CN/zh-CN.i18n.json',
-                    'i18n/zh-TW/zh-TW.i18n.json',
-                    'i18n/project-tap.i18n'           
-                 ]);
-	api.use('tap:i18n');
-	api.imply('tap:i18n');
-        
+        'i18n/en/en.i18n.json',
+        'i18n/fr/fr.i18n.json',
+        'i18n/zh-CN/zh-CN.i18n.json',
+        'i18n/zh-TW/zh-TW.i18n.json',
+        'i18n/project-tap.i18n'
+    ]);
+    api.use('tap:i18n');
+    api.imply('tap:i18n');
+
     api.export('Smartix');
 });
