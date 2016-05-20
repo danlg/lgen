44718bd1f703a0b5e92ef32f45a0b330dc19d23c
Terence Ng <terenceng2010@gmail.com>
2016-05-20 16:44:21 +0800

    can filter by user's roles

--- app/packages/admin/client/templates/users/list/index.js
+++ app/packages/admin/client/templates/users/list/index.js
@@ -12,11 +12,18 @@ UsersIndex = new EasySearch.Index({
             // selector contains the default mongo selector that Easy Search would use
             var selector = this.defaultConfiguration().selector(searchObject, options, aggregation);
 
-            // modify the selector to only match documents where region equals "New York"
-            //selector.type = 'class';
+            if(options.search.props.schoolNamespace){
+                // filter for the roles if set
+                if (options.search.props.role && options.search.props.role !== 'all') {            
+                    selector.roles = {
+                        [options.search.props.schoolNamespace] : [options.search.props.role]
+                    }
+                }                
+            }
 
+            
             return selector;
         }
     }),
-    defaultSearchOptions : {limit: 50}
+    defaultSearchOptions : {limit: 50},
 });
