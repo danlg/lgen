eba5dfc903c3c52e1fd253845d79ada6900050aa
Terence Ng <terenceng2010@gmail.com>
2016-05-20 15:00:05 +0800

    add grade, swap fields position, sort by last name

--- app/packages/admin/client/templates/users/list/index.js
+++ app/packages/admin/client/templates/users/list/index.js
@@ -2,7 +2,21 @@
 // https://github.com/matteodem/meteor-easy-search/issues/445
 UsersIndex = new EasySearch.Index({
     collection: Meteor.users,
-    fields: ['profile.firstName', 'profile.lastName'],
-    engine: new EasySearch.Minimongo(),
+    fields: ['profile.firstName', 'profile.lastName','roles','grade'],
+    engine: new EasySearch.Minimongo({
+        sort: function(searchObject,options){
+            return { 'profile.lastName' : 0 }
+        },
+        selector: function (searchObject, options, aggregation) {
+            
+            // selector contains the default mongo selector that Easy Search would use
+            var selector = this.defaultConfiguration().selector(searchObject, options, aggregation);
+
+            // modify the selector to only match documents where region equals "New York"
+            //selector.type = 'class';
+
+            return selector;
+        }
+    }),
     defaultSearchOptions : {limit: 50}
 });
