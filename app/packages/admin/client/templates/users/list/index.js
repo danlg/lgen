9c8c6d936f67b1eac98b56b606bb50fb6894ab2f
Terence Ng <terenceng2010@gmail.com>
2016-05-23 14:21:56 +0800

    make role filter works even user has multiple roles #427

--- app/packages/admin/client/templates/users/list/index.js
+++ app/packages/admin/client/templates/users/list/index.js
@@ -14,13 +14,21 @@ UsersIndex = new EasySearch.Index({
 
             if(options.search.props.schoolNamespace){
                 // filter for the roles if set
-                if (options.search.props.role && options.search.props.role !== 'all') {            
-                    selector.roles = {
-                        [options.search.props.schoolNamespace] : [options.search.props.role]
-                    }
+                if (options.search.props.role && options.search.props.role !== 'all') {  
+                    
+                    //https://github.com/matteodem/meteor-easy-search/issues/239
+                    //http://stackoverflow.com/questions/16002659/mongodb-how-to-query-nested-objects
+                    //https://docs.mongodb.com/v3.0/reference/operator/query/and/
+                    selector['$and'] = [];
+                    selector['$and'].push( { '$or': selector['$or'] }); 
+                    delete selector['$or'];
+                    selector['$and'].push( { ['roles.'+options.search.props.schoolNamespace] : options.search.props.role }  ) ;
+
                 }                
             }
 
+            //TO examine the above mongo selector,un-comment the below console log
+            //console.log(selector);
             
             return selector;
         }
