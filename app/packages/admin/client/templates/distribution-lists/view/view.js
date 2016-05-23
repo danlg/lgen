c9958140e9fc1780d426ef5de3c254668498b4aa
Terence Ng <terenceng2010@gmail.com>
2016-05-24 00:35:40 +0800

    change to table add/remove in distribution list

--- app/packages/admin/client/templates/distribution-lists/view/view.js
+++ app/packages/admin/client/templates/distribution-lists/view/view.js
@@ -7,6 +7,7 @@ Template.AdminDistributionListView.onCreated(function () {
         var schoolName = Router.current().params.school;
         self.subscribe('schoolInfo', schoolName, function (err, res) {
             var schoolId = Smartix.Accounts.School.getNamespaceFromSchoolName(schoolName);
+            self.namespace = schoolId;
             self.subscribe('smartix:distribution-lists/listByCode', currentCode, function (error, res) {
                 self.subscribe('smartix:accounts/basicInfoOfAllUsersInNamespace', schoolId, function (err, res) {
                     
@@ -25,6 +26,9 @@ Template.AdminDistributionListView.onCreated(function () {
             });
         });
     }
+    
+    this.usersChecked = new ReactiveVar([]);
+    this.doingOperations = new ReactiveVar(false);
 });
 
 Template.AdminDistributionListView.helpers({
@@ -52,7 +56,53 @@ Template.AdminDistributionListView.helpers({
             class: "form-control",
             id: "AdminDistributionListView__add-user-input"
         }
-    }
+    },
+    doingOperations:function(){
+    return Template.instance().doingOperations.get();   
+    },
+    getUserEmail:function(){
+        if(this.emails){
+        return this.emails[0].address;
+        }
+    },
+    getUserRoles:function(){
+        var schoolUsername = Router.current().params.school;
+        var schoolNamespace = Smartix.Accounts.School.getNamespaceFromSchoolName(schoolUsername);
+        if(schoolNamespace){
+            let role = "";
+            if(this.roles) {
+                //tod this more clever
+                role = this.roles[schoolNamespace].toString();
+                if (role === "student") { role = "Student" }
+                if (role === "parent") { role = "Parent" }
+                if (role === "teacher") { role = "Teacher" }
+                if (role === "admin") { role = "Admin" }
+            }
+            return role;
+        }            
+
+    },
+    getCurrentSchoolName:function(){
+        return Router.current().params.school;
+    },
+    getUserId:function(){
+        return this._id;
+    },
+    isUserChecked:function(){
+        //console.log(this._id )
+        //console.log(Template.instance().usersChecked.get());
+        return (  Template.instance().usersChecked.get().indexOf(this._id) !== -1 ) ? "checked" : "";
+    },
+    totalUserCount:function(){
+        return Meteor.users.find( {},{ fields:{ _id: 1} } ).count();
+    },
+    totalSelectUserCount:function(){
+        return Template.instance().usersChecked.get().length;
+    },
+    showOptions:function(){
+        return Template.instance().usersChecked.get().length > 0 ;
+        
+    }    
 });
 
 Template.AdminDistributionListView.events({
@@ -89,5 +139,91 @@ Template.AdminDistributionListView.events({
                 // console.log(res);
             })
         }
-    }
+    },
+    'click .add-users-to-distribution-list':function(event,template){
+        let userIdsToAdd = template.usersChecked.get()
+        let listOfUsers = Meteor.users.find({_id:{$in: userIdsToAdd }}).fetch();
+        var currentList = Router.current().params.code;       
+        let listOfUserNames = listOfUsers.map(function(eachUserObj){
+            return eachUserObj.profile.firstName + " " + eachUserObj.profile.lastName;
+        });
+
+        Meteor.call('smartix:distribution-lists/addUsersByListName', currentList, userIdsToAdd, function (err, res) {
+            toastr.info(listOfUserNames.toString() + ' added to distributionList');
+            // console.log(err);
+            // console.log(res);
+        })               
+    },
+    'click .remove-users-from-distribution-list':function(event,template){
+        let userIdsToRemove = template.usersChecked.get()
+     
+        let listOfUsers = Meteor.users.find({_id:{$in: userIdsToRemove }}).fetch();
+        var currentList = Router.current().params.code;     
+        let listOfUserNames = listOfUsers.map(function(eachUserObj){
+            return eachUserObj.profile.firstName + " " + eachUserObj.profile.lastName;
+        })
+        
+        Meteor.call('smartix:distribution-lists/removeUsersByListName', currentList, userIdsToRemove, function (err, res) {
+            toastr.info(listOfUserNames.toString() + ' removed from distributionList');            
+            // console.log(err);
+            // console.log(res);
+        })        
+      
+    },    
+    'click .school-directory-user-checkbox':function(event,template){
+        
+        if( $(event.target).prop('checked') ) {
+           let latestArray = template.usersChecked.get();
+           log.info($(event.target).val());
+           latestArray.push( $(event.target).val() );
+           
+           template.usersChecked.set( latestArray  );            
+        }else{
+           let latestArray = template.usersChecked.get();
+           log.info($(event.target).val());
+           lodash.pull(latestArray, $(event.target).val());
+                       
+           template.usersChecked.set( latestArray  );              
+        }      
+    },
+   'click .select-all-users-btn':function(event,template){
+     var userObjects = Meteor.users.find( {},{ fields:{ _id: 1} } ).fetch();
+     var userIds = lodash.map(userObjects,"_id");
+     let latestArray = template.usersChecked.set(userIds);  
+   },
+   'click .select-all-users-current-page-btn':function(event,template){
+      $('.school-directory-user-checkbox').each(function(index){
+          if( $(this).prop('checked') ){
+            //do nothing
+          }else{
+           let latestArray = template.usersChecked.get();
+           log.info($(this).val());
+           latestArray.push( $(this).val() );
+           
+           template.usersChecked.set( latestArray  );                
+          }
+      });
+   },
+   'click .deselect-all-users-current-page-btn':function(event,template){
+      $('.school-directory-user-checkbox').each(function(index){
+          if( $(this).prop('checked') ){
+           let latestArray = template.usersChecked.get();
+           log.info($(this).val());
+           lodash.pull(latestArray, $(this).val());
+                       
+           template.usersChecked.set( latestArray  );                 
+          }else{
+            //do nothing
+          }
+      });
+   },   
+   'click .deselect-all-users-btn':function(event,template){
+      template.usersChecked.set([]);
+   },
+   'click .filter-by-role-btn':function(event,template){
+       var chosenRole =  $(event.target).data('role');
+        DistributionListUsersIndex.getComponentMethods().addProps('schoolNamespace', template.namespace);
+        DistributionListUsersIndex.getComponentMethods().addProps('role', chosenRole);
+               
+   }      
 });
