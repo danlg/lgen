fe20ebab2e2b776fed9d4d1406295108d9539c8c
Terence Ng <terenceng2010@gmail.com>
2016-05-26 12:30:03 +0800

    can pick and change color, school name and color in one row, add city, add communication manager, change call to action, change school info, personal info header text,change school name placeholder text,

--- app/packages/accounts/client/templates/school_signup/school_signup.js
+++ app/packages/accounts/client/templates/school_signup/school_signup.js
@@ -3,6 +3,9 @@ Template.SchoolSignup.onCreated(function(){
    this.chosenRole = new ReactiveVar('');
    this.chosenNumberOfStudent = new ReactiveVar(0);  
    this.mySchoolName = new ReactiveVar(''); 
+   
+   this.inputBackgroundColor = new ReactiveVar('#811719');
+   this.inputTextColor = new ReactiveVar('#FFFFFF');
 });
 
 Template.SchoolSignup.helpers({
@@ -11,12 +14,47 @@ Template.SchoolSignup.helpers({
     },
     mySchoolNameForSimulatedScreen:function(){
         if( Template.instance().mySchoolName.get() === '' ){
-             return "Your school name";
+             return "My remarkable school";
         }else{
             return Template.instance().mySchoolName.get();     
            
         }
-    },    
+    },
+    getInputBackgroundColor : function(){
+      return Template.instance().inputBackgroundColor.get();
+    },
+    customizeTheme:function(){
+        var schoolBackgroundColor = Template.instance().inputBackgroundColor.get();
+        var schoolTextColor       = Template.instance().inputTextColor.get();
+        console.log(schoolBackgroundColor,schoolTextColor);
+        var customStyle = `
+            
+        <style>
+                #example-forms-floating-labels .bar.bar-positive, .bar.bar-positive, .bar.bar-stable,
+                .button.button-positive.active, .button.button-positive {
+                    border-color: ${schoolBackgroundColor};
+                    background-color: ${schoolBackgroundColor};
+                    background-image: linear-gradient(0deg, ${schoolBackgroundColor}, ${schoolBackgroundColor} 50%, transparent 50%);
+                    color: ${schoolTextColor};
+                }
+                
+                .bar.bar-stable .title, .bar-stable .button.button-clear{
+                color: ${schoolTextColor};
+                }
+                
+                .card.square-card .mask{
+                    background-color: ${schoolBackgroundColor}; 
+                }
+                
+                .device-preview-backdrop {
+                    background-color: ${schoolBackgroundColor};                         
+                }       
+            </style>
+        `;
+        
+        return customStyle;
+    }
+       
 })
 
 Template.SchoolSignup.events({
@@ -36,9 +74,17 @@ Template.SchoolSignup.events({
       $(event.currentTarget).addClass('chosen-number-of-student');          
       template.chosenNumberOfStudent.set($(event.currentTarget).data('number'));
   },
-  'click .createBtn':function(event,template){
-      //TODO: really pass data to page 2 , form input checking 
+  'click .start-my-trial-btn':function(event,template){
+      //TODO: really pass data to page 2 , form input checking
+      
+      
+      
       console.log('route to page 2');
       Router.go('SchoolSignupPage2');
+  },
+  'change #school-background-color':function(event,template){
+      
+      template.inputBackgroundColor.set(  $(event.target).val() );
+      
   }
 });
