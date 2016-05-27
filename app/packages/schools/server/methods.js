d426ec2f985f70d0d40433d00c1117351633c48c
Terence Ng <terenceng2010@gmail.com>
2016-05-27 15:50:50 +0800

    change to use FROM_EMAIL , FEEDBACK_EMAIL

--- app/packages/schools/server/methods.js
+++ app/packages/schools/server/methods.js
@@ -84,10 +84,10 @@ Meteor.methods({
             Meteor.defer(function(){
                 Email.send(
                     { 
-                        from: 'contactemail@gosmartix.com',
-                        to:   'terenceng2010@gmail.com',
+                        from: Meteor.settings.FROM_EMAIL,
+                        to:   Meteor.settings.FEEDBACK_EMAIL,
                         subject : 'new lead' + options.lead.firstName + ' ' + options.lead.lastName,
-                        text: options.lead.toString()
+                        html: JSON.stringify(options)
                     
                     }
                 );                
