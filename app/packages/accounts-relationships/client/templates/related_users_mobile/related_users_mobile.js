d266e74fff4f1828d0ddb90b7590c5b1382b5178
Daniel Rosenberg <dan@littlegenius.io>
2016-05-30 19:53:48 +0800

    simplify relationship logic and avoid duplicate info

--- app/packages/accounts-relationships/client/templates/related_users_mobile/related_users_mobile.js
+++ app/packages/accounts-relationships/client/templates/related_users_mobile/related_users_mobile.js
@@ -2,44 +2,37 @@ Template.RelatedUsersMobile.helpers({
    relationships:function(){
      return Smartix.Accounts.Relationships.Collection.find();
    },
-   getParentNameById: function(userid){
+   getFirstAndLastNameById: function(userid){
        var usr =  Meteor.users.findOne(userid);
        if(usr) {
            return usr.profile.firstName + " " + usr.profile.lastName;
        }
    },
-   getChildNameById:function(userid){
-       var usr =  Meteor.users.findOne(userid);
-       return usr.profile.firstName + " " + usr.profile.lastName;       
+   userIsChild:function(){
+       //var isChild = (this.child ===  Router.current().params.uid);
+       //log.info("userIsChild:"+ Router.current().params.uid + "=" +  isChild);
+       return (this.child === Router.current().params.uid);
    },
-   userIsChild:function(userid){
-       if(Meteor.userId() == userid ){
-           return true;
-       }else{
-           return false;
-       }
-   },
-   userIsParent:function(userid){
-       if(Meteor.userId() == userid ){
-           return true;
-       }else{
-           return false;
-       }
-   },
-   userIsChildOrParent:function(){
-       if(this.child ==Meteor.userId() || this.parent == Meteor.userId()){
-           return true;
-       }else{
-           return false;
-       }
+
+   userIsParent:function(){
+       //var isParent = (this.parent ===  Router.current().params.uid);
+       //log.info("userIsParent:"+ Router.current().params.uid + "=" + isParent);
+       return (this.parent === Router.current().params.uid);
    }
+
+   // userIsChildOrParent:function(userid){
+   //   return ( Template.RelatedUsersMobile.__helpers.get('userIsParent').call() )
+   //      ||  ( Template.RelatedUsersMobile.__helpers.get('userIsChild').call() );
+   // },
+   // relationshipName:function() {
+   //     //Mother, Father,...so can be translated
+   //     return TAPi18n.__(this.name);
+   // }
 });
 
 Template.RelatedUsersMobile.events({
   'click .remove-relationship-btn': function (e) {
     var relationshipId = $(e.target).data("relationshipId");
-   
     Meteor.call('smartix:accounts-relationships/removeRelationship',relationshipId);
-
   }
 });
