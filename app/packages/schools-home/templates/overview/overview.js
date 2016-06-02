c853bd95f3b3cf25cb4d5c8e5ba699717afc8297
Daniel Rosenberg <dan@littlegenius.io>
2016-06-02 12:32:12 +0800

    smart improvement on the text and layout of class view , import student and localization

--- app/packages/schools-home/templates/overview/overview.js
+++ app/packages/schools-home/templates/overview/overview.js
@@ -19,66 +19,46 @@ Template.MobileSchoolHome.helpers({
     
     schoolFullName:function(){
         var schoolLogoId;
-        
         var schoolDoc = SmartixSchoolsCol.findOne({                                                    
             username: Router.current().params.school                                                                     
         });
-        
         if(schoolDoc){
          return schoolDoc.name;             
         }
-      
     },
+
     getCurrentSchoolName: function() {
         return Router.current().params.school;
     },
     getSlidingNews:function(){
         var newsgroupsIds = [];
-        
-        
         var newsgroupsByUserArray =  Smartix.Groups.Collection.find({ type: 'newsgroup', users: Meteor.userId() }).fetch(); 
         var newsgroupsByUserArrayIds = lodash.map(newsgroupsByUserArray,'_id');
-        
         var distributionListsUserBelong = Smartix.Groups.Collection.find({type: 'distributionList', users: Meteor.userId() }).fetch();
         var distributionListsUserBelongIds = lodash.map(distributionListsUserBelong,'_id');
-        
         //console.log('distributionListsUserBelongIds',distributionListsUserBelongIds);
-        
         var newsgroupsBydistributionLists =  Smartix.Groups.Collection.find({ type: 'newsgroup', distributionLists: {$in : distributionListsUserBelongIds } , optOutUsersFromDistributionLists :{  $nin : [Meteor.userId()] } }).fetch();      
         var newsgroupsBydistributionListsIds = lodash.map(newsgroupsBydistributionLists,'_id');
-        
         //console.log('newsgroupsBydistributionListsIds',newsgroupsBydistributionListsIds);
-        
         newsgroupsIds = newsgroupsIds.concat(newsgroupsByUserArrayIds,newsgroupsBydistributionListsIds);
-        
         //console.log('newsgroupsIds',newsgroupsIds);
-        
         Template.instance().canGetSlidNews.set(true);
-        
         return Smartix.Messages.Collection.find({$or:[
             {
                 group: { $in: newsgroupsIds },
                 hidden : false,
-                deletedAt:"",
-
+                deletedAt:""
             },
             {
                 group: { $in: newsgroupsIds },
                 hidden: false,
-                deletedAt: { $exists: false },
-                 
+                deletedAt: { $exists: false }
             }
         ]}
         , {sort: {createdAt: -1 }, reactive: false }
-        );  
-                
-
-          
-
-
-
-
+        );
     },
+    
     needMaskImageFallback:function(){
       return (document.documentElement.style['-webkit-mask-image'] !== undefined) ? "" : "mask-image-fallback"
     },
