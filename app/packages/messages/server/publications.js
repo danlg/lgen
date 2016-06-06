ff9fe33a5acb39aec43ecd6fed69ebb99574b7de
Daniel Rosenberg <dan@littlegenius.io>
2016-06-07 07:48:46 +0800

    remove verbose message

--- app/packages/messages/server/publications.js
+++ app/packages/messages/server/publications.js
@@ -1,31 +1,25 @@
 Meteor.publish('smartix:messages/messagesById', function (id) {
-    
-    // Check for permission
-  return Smartix.Messages.find({
-    _id: id
-  });
+  // Check for permission
+  return Smartix.Messages.find({ _id: id });
 });
 
 Meteor.publish('smartix:messages/groupMessages', function (groupId) {
-  
   //find Groups that is:
   //1. in the current namespace and 
   //2a. either current user is one of the users in the group
   //2b. or     current user is one of the admins in the group
-  log.info('smartix:messages/groupMessages',groupId);
-  
+  //log.info('smartix:messages/groupMessages',groupId);
   return Smartix.Messages.Collection.find(
     { group: groupId }
   );
 });
 
 Meteor.publish('smartix:messages/latestMessageEachGroups', function (groupIds) {
-  
   //find Groups that is:
   //1. in the current namespace and 
   //2a. either current user is one of the users in the group
   //2b. or     current user is one of the admins in the group
-  log.info('smartix:messages/latestMessageEachGroups');
+  //log.info('smartix:messages/latestMessageEachGroups');
   return Smartix.Messages.Collection.find(
     { group:{$in: groupIds} }
   );
