11980b98368ad7369070f65f1aefe561ff6bb01b
Daniel Rosenberg <dan@littlegenius.io>
2016-06-16 14:19:00 +0800

    bump build version to 1.2.2 build 243

--- app/packages/smartix-core/client/templates/about/about.js
+++ app/packages/smartix-core/client/templates/about/about.js
@@ -7,7 +7,7 @@ Template.About.helpers({
     //require plugin cordova-plugin-compile-time
     //http://eclipsesource.com/blogs/2015/04/07/an-apache-cordova-hook-to-auto-bump-ios-cfbundleversion-and-android-versioncode/
     //https://gist.github.com/hstaudacher/d78b154509e2783cfcc2 - timestamp_version_bump.js
-    var version = "1.2.2 build 242";
+    var version = "1.2.2 build 243";
     if (Meteor.isCordova) {
       //version = window.cordova.compileTime.version; //now broken with carmel branch
       return version;
