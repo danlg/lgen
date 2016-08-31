fcb4e25c53f354489ec90738607422bd6385be6f
Daniel Rosenberg <dan@littlegenius.io>
2016-08-31 15:38:27 +0530

    add extra debug info for show /hide news - [App] Show / Hide / Delete news doesn't work #571

--- app/packages/news/server/methods.js
+++ app/packages/news/server/methods.js
@@ -1,20 +1,37 @@
 Meteor.methods({
     'smartix:news/showMessage': function(msgId) {
-        //log.info("smartix:news/showMessage", msgId);
-        var targetMsg = Smartix.Messages.Collection.findOne(msgId);
+        log.info("smartix:news/showMessage '" + msgId + "'");
+        //log.info("smartix:news/showMessage typeof '" + typeof(msgId) + "'");
+        //log.info("smartix:news/showMessage typeof str'" + typeof(msgId.toString()) + "'");
+        //let targetMsgCursor = Smartix.Messages.Collection.find(msgId.toString());
+        //log.info("smartix:news/showMessage:cursor.count", targetMsgCursor.count());
+        //log.info("smartix:news/showMessage:cursor", targetMsgCursor);
+        var targetMsg = Smartix.Messages.Collection.findOne(msgId.toString());
         if (targetMsg) {
             if (Smartix.Class.isClassAdmin(Meteor.userId(), targetMsg.group)) {
-                return Smartix.Messages.showMessage(msgId);
+                return Smartix.Messages.showMessage(msgId.toString());
             }
+            else{
+                log.info("smartix:news/showMessage:cannot show/not admin", msgId);
+            }
+        }
+        else{
+            log.info("smartix:news/showMessage:cannot show/not found", msgId);
         }
     },
     'smartix:news/hideMessage': function(msgId) {
-        //log.info("smartix:news/hideMessage", msgId);
-        var targetMsg = Smartix.Messages.Collection.findOne(msgId);
+        log.info("smartix:news/hideMessage", msgId.toString());
+        var targetMsg = Smartix.Messages.Collection.findOne(msgId.toString());
         if (targetMsg) {
             if (Smartix.Class.isClassAdmin(Meteor.userId(), targetMsg.group)) {
-                return Smartix.Messages.hideMessage(msgId);
+                return Smartix.Messages.hideMessage(msgId.toString());
             }
+            else{
+                log.info("smartix:news/hideMessage:cannot hide/not admin", msgId);
+            }
+        }
+        else{
+            log.info("smartix:news/hideMessage:cannot hide/not found", msgId);
         }
     },
 
