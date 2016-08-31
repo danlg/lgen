572ae23daf0affeea7f8ee38726f6731c18a308d
Daniel Rosenberg <dan@littlegenius.io>
2016-08-31 15:31:58 +0530

    change notification to be more selective and publish only for a given school and userId - #606

--- app/packages/smartix-notifications/publications.js
+++ app/packages/smartix-notifications/publications.js
@@ -1,8 +1,19 @@
 /*! Copyright (c) 2015 Little Genius Education Ltd.  All Rights Reserved. */
 
-Meteor.publish('notifications', function () {
+Meteor.publish('notifications', function (schoolId) {
     return Notifications.find({
       userId: this.userId,
+      namespace: schoolId,
       hasRead: false
-    });    
+      //TODO add here the event Type for a finer granularity
+    });
 });
+
+//db.getCollection('notifications').distinct("eventType")
+// [
+//     "newchatmessage",
+//     "newclassmessage",
+//     "newclasscomment",
+//     "newnewsgroupmessage",
+//     "attendance"
+// ]
