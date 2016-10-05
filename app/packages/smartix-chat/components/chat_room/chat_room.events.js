3fa529214e50f5d755c94f8c31151db374b3ba56
Daniel Rosenberg <dan@littlegenius.io>
2016-10-05 08:30:00 +0200

    [App] Save photo to library #633 - make it work for iOS and Android (small image)

--- app/packages/smartix-chat/components/chat_room/chat_room.events.js
+++ app/packages/smartix-chat/components/chat_room/chat_room.events.js
@@ -10,28 +10,30 @@ Template.imageModal.events({
 	// Here handling chat image long press - on long press showing popup for save/cancel the image to mobile gallery - Rajit Deligence
 	//'contextmenu #dt-image-chat': function (e) {
 	// not used yet to debug
+	//cordova-base64-to-gallery@4.1.1
 	'click #imageGallery': function(event, template){
 		var img = document.getElementById('imageHolder');
-		log.info("Test", img);
+		//log.info("Test", img);
 		function getBase64ImageHolder(image) {
 			var imgSrc = image.getAttribute('src');
-			log.info(imgSrc);
+			log.info("getBase64ImageHolder/imgSrc="+imgSrc);
 			imgSrc = imgSrc.split('?')[0];
-			var saveImage = blobUtil.imgSrcToDataURL(imgSrc, 'image/jpeg', { crossOrigin: 'Anonymous' }).then(function (dataURL) {
-				log.info("Data URL", dataURL);
+			//https://github.com/nolanlawson/blob-util#imgSrcToBlob API
+			var saveImage = blobUtil.imgSrcToDataURL(imgSrc, 'image/png', { crossOrigin: 'Anonymous' }, 1.0).then(function (dataURL) {
+				//log.info("Data URL", dataURL);
 				var params = { data: dataURL, quality: 100 };
 				window.imageSaver.saveBase64Image(params,
 					function (result) {
-						log.info('result ' + result);
-						toastr.info("Image saved successfully");
+						log.info('window.imageSaver.saveBase64Image.result OK' + result);
+						toastr.info("Image saved successfully in photo album");
 					},
 					function (error) {
-						log.error('error ' + error);
+						log.error('window.imageSaver.saveBase64Image.error ' + error);
 						toastr.error("Error saving image");
 					}
 				);			// log.info(schoolbannerSource, dataURL);
 			}).catch(function (err) {
-				log.error(err);
+				log.error("cannot save image", err);
 			});               //.replace(/^data:image\/(png|jpg);base64,/, "");
 		}
 		var getBase64 = getBase64ImageHolder(img);
