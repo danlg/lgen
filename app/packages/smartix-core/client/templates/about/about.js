4d67297f3ee530fa9e27df48c6b4482641c000a0
Daniel Rosenberg <dan@littlegenius.io>
2016-10-22 10:04:01 +0200

    bump version 1.2.4 build 401 (incl. iOS 10 websocket fix & iOS permissions in mobile-config.js with App.appendToConfig)

--- app/packages/smartix-core/client/templates/about/about.js
+++ app/packages/smartix-core/client/templates/about/about.js
@@ -7,7 +7,7 @@ Template.About.helpers({
     //require plugin cordova-plugin-compile-time
     //http://eclipsesource.com/blogs/2015/04/07/an-apache-cordova-hook-to-auto-bump-ios-cfbundleversion-and-android-versioncode/
     //https://gist.github.com/hstaudacher/d78b154509e2783cfcc2 - timestamp_version_bump.js
-    var version = "1.2.4 build 400";
+    var version = "1.2.4 build 401";
     return version;
     // if (Meteor.isCordova) {
     //   //version = window.cordova.compileTime.version; //now broken with carmel branch
